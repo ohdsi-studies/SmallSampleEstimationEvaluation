4c6dab1eb9c0cd95b4140955356db005a3885d6e
Schuemie <MSCHUEMI@its.jnj.com>
2023-10-02 09:17:58 +0200

    Some work on plots for symposium poster

--- extras/SymposiumPlots.R
+++ extras/SymposiumPlots.R
@@ -1,7 +1,9 @@
-# Plots for the OHDSI Global Symposium collaborator showcase submission
+# Plots for the Methods Workgroup meeting
 library(dplyr)
 library(ggplot2)
 
+# plotsAndTablesFolder <- "C:/Users/mschuemi/OneDrive - JNJ/home/Research/SmallCountConfounderAdjustment"
+plotsAndTablesFolder <- file.path("d:/SmallSampleEstimationEvaluation_mdcd", "plotsAndTables")
 metrics <- bind_rows(
   readRDS(file.path("d:/SmallSampleEstimationEvaluation_mdcd", "CombinedMetrics.rds")),
   readRDS(file.path("d:/SmallSampleEstimationEvaluation_mdcr", "CombinedMetrics.rds")),
@@ -9,10 +11,20 @@ metrics <- bind_rows(
 )
 metrics <- metrics %>%
   mutate(comparison = gsub("Lisinporil ", "Lisinopril ", comparison))
+psMetrics <- bind_rows(
+  readRDS(file.path("d:/SmallSampleEstimationEvaluation_mdcd", "CombinedPSMetrics.rds")),
+  readRDS(file.path("d:/SmallSampleEstimationEvaluation_mdcr", "CombinedPsMetrics.rds")),
+  readRDS(file.path("d:/SmallSampleEstimationEvaluation_optum_ehr", "CombinedPsMetrics.rds"))
+)
+psMetrics <- psMetrics %>%
+  mutate(comparison = gsub("Lisinporil ", "Lisinopril ", comparison))
+saveRDS(metrics, file.path(plotsAndTablesFolder, "metrics.rds"))
+saveRDS(psMetrics, file.path(plotsAndTablesFolder, "psMetrics.rds"))
 
-plotsAndTablesFolder <- file.path("d:/SmallSampleEstimationEvaluation_mdcd", "plotsAndTables")
+# metrics <- readRDS(file.path(plotsAndTablesFolder, "metrics.rds"))
+# psMetrics <- readRDS(file.path(plotsAndTablesFolder, "psMetrics.rds"))
 
-sampleSizes <- c(4000, 2000, 1000, 500, 250)
+sampleSizes <- c(4000, 2000, 1000, 500, 250, 125)
 x <- tibble(
   sampleSize = c(20000, sampleSizes),
   x = seq_len(length(sampleSizes) + 1)
@@ -20,30 +32,54 @@ x <- tibble(
 
 # EASE -----------------------------
 vizData <- metrics %>%
-  filter(analysisId %in% c(1, 3, 4, 5) & calibrated == FALSE) %>%
-  mutate(x = if_else(psMethod == "PS stratification", x + 0.2, x - 0.2)) %>%
-  mutate(adjustment = sprintf("%s using %s model", psMethod, tolower(psModel))) %>%
+  filter(calibrated == FALSE) %>%
+  mutate(x = round(x)) %>%
+  mutate(x = case_when(psModel == "Local" & psMethod == "PS stratification" ~ x - 0.32,
+                       psModel == "Local" & psMethod == "PS 1-on-1 matching" ~ x - 0.16,
+                       psModel == "Global" & psMethod == "PS stratification" ~ x,
+                       psModel == "Global" & psMethod == "PS 1-on-1 matching" ~ x + 0.16,
+                       psModel == "None" ~ x + 0.32)) %>%
+  mutate(adjustment = sprintf("%s using %s model", psMethod, tolower(psModel)),
+         comparison = gsub("vs ", "vs\n", comparison)) %>%
   select(comparison, x, ease, easeCi95Lb, easeCi95Ub, adjustment, psMethod, psModel, adjustment, database)
+vizData$psMethod <- factor(vizData$psMethod, levels = rev(c("No PS adjustment",
+                                                            "PS 1-on-1 matching",
+                                                            "PS stratification")))
+vizData$psModel <- factor(vizData$psModel, levels = c("Local",
+                                                      "Global",
+                                                      "None"))
 
 ggplot(vizData, aes(x = x, y = ease, ymin = easeCi95Lb, ymax = easeCi95Ub, group = adjustment, color = psModel)) +
-  geom_vline(xintercept = 0.5 + 2:5, color = "white") +
-  geom_errorbar() +
-  geom_point(aes(shape = psMethod), size = 2) +
+  # geom_hline(yintercept = 0.25, linetype = "dashed", alpha = 0.5) +
+  geom_vline(xintercept = x$x + 0.5, color = gray(0.8)) +
+  geom_hline(yintercept = 0) +
+  geom_errorbar(alpha = 0.8) +
+  geom_point(aes(shape = psMethod), size = 2, alpha = 0.8) +
+  scale_color_manual(values = c("#69AED5", "#336B91", "#EB6622")) +
   scale_x_continuous("Sample size per site", breaks = x$x, labels = x$sampleSize, minor_breaks = NULL) +
   scale_y_continuous("Expected Absolute Systematic Error (EASE) with 95% credible interval") +
   coord_cartesian(ylim = c(0, 0.5)) +
   labs(color = "Propensity model", shape = "Adjustment strategy") +
   facet_grid(comparison~database) +
   theme(legend.position = "top",
-        legend.direction = "vertical",
-        panel.grid.major.x = element_blank())
-ggsave(file.path(plotsAndTablesFolder, "EASE_all.png"), width = 8, height = 9.5, dpi = 300)
+        legend.direction = "horizontal",
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(color = gray(0.8)),
+        panel.grid.major.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_line(color = gray(0.8)),
+        strip.background = element_blank())
+
+ggsave(file.path(plotsAndTablesFolder, "EASE_crude_all.png"), width = 10, height = 6, dpi = 300)
 
 # Precision after calibration --------------------------
 vizData <- metrics %>%
-  filter(analysisId %in% c(1, 3, 4, 5) & calibrated == TRUE) %>%
-  mutate(x = if_else(psMethod == "PS stratification", x + 0.2, x - 0.2)) %>%
-  mutate(adjustment = sprintf("%s using %s model", psMethod, tolower(psModel))) %>%
+  filter(calibrated == TRUE) %>%
+  mutate(x = case_when(psMethod == "PS stratification" ~ x + 0.3, 
+                       psMethod == "No PS adjustment" ~ x - 0.3,
+                       .default = x - 0.1)) %>%
+  mutate(adjustment = sprintf("%s using %s model", psMethod, tolower(psModel)),
+         comparison = gsub("vs ", "vs\n", comparison)) %>%
   select(comparison, x, meanP, adjustment, psMethod, psModel, adjustment, database)
 
 ggplot(vizData, aes(x = x, y = meanP, group = adjustment, color = psModel)) +
@@ -54,26 +90,41 @@ ggplot(vizData, aes(x = x, y = meanP, group = adjustment, color = psModel)) +
   # coord_cartesian(ylim = c(0, 0.5)) +
   labs(color = "Propensity model", shape = "Adjustment strategy") +
   facet_grid(comparison~database) +
-  theme(legend.position = "top",
+  theme(legend.position = "right",
         legend.direction = "vertical",
         panel.grid.major.x = element_blank())
-ggsave(file.path(plotsAndTablesFolder, "Precision_afer_calibration_all.png"), width = 8, height = 9.5, dpi = 300)
+ggsave(file.path(plotsAndTablesFolder, "Precision_afer_calibration_crude_all.png"), width = 14, height = 7, dpi = 300)
 
-# EASE before PS adjustment -----------------------------------------
-metricsLargeSample <- bind_rows(
-  readr::read_csv(file.path("d:/SmallSampleEstimationEvaluation_mdcd", "Metrics_LargeSample.csv")),
-  readr::read_csv(file.path("d:/SmallSampleEstimationEvaluation_mdcr", "Metrics_LargeSample.csv")),
-  readr::read_csv(file.path("d:/SmallSampleEstimationEvaluation_optum_ehr", "Metrics_LargeSample.csv"))
-)
-vizData <- metricsLargeSample %>%
-  filter(calibrated == FALSE & analysisId == 2) %>%
-  mutate(group = case_when(ease < 0.1 ~ "L", ease < 0.25 ~ "M", .default = "H")) %>%
-  mutate(label = sprintf("%0.2f (%0.2f-%0.2f) %s", ease, easeCi95Lb, easeCi95Ub, group)) %>%
-  inner_join(metrics %>%
-               distinct(targetId, comparatorId, comparison),
-             by = join_by(targetId, comparatorId)) %>%
-  select(comparison, database, label) %>%
-  arrange(database, comparison)
-vizData <- tidyr::pivot_wider(vizData, names_from = "database", values_from = "label")
-readr::write_excel_csv(vizData, file.path("d:/SmallSampleEstimationEvaluation_mdcd", "crude_ease.csv"))
 
+# Balance -----------------------------
+vizData <- psMetrics %>%
+  # filter(analysisId == 1) %>%
+  mutate(comparison = gsub("vs ", "vs\n", comparison)) %>%
+  mutate(x = case_when(psMethod == "PS stratification" ~ x + 0.2, 
+                       .default = x - 0.2)) %>%
+  select(comparison, x, maxSdmMin, maxSdmP25, maxSdmMedian, maxSdmP75, maxSdmMax, psMethod, database)
+
+ggplot(vizData, aes(x = x, y = maxSdmMedian, group = x, color = psMethod, fill = psMethod)) +
+  geom_vline(xintercept = x$x + 0.5, color = gray(0.8)) +
+  geom_hline(yintercept = 0.1, linetype = "dashed") +
+  geom_boxplot(
+    aes(ymin = maxSdmMin, lower = maxSdmP25, middle = maxSdmMedian, upper = maxSdmP75, ymax = maxSdmMax),
+    stat = "identity",
+    alpha = 0.3
+  ) +
+  scale_x_continuous("Sample size per site", breaks = x$x, labels = x$sampleSize, minor_breaks = NULL) +
+  scale_y_continuous("Maximum absolute standardized difference of mean (SDM)") +
+  scale_color_manual(values=c("#FCC90C", "#336B91")) +
+  scale_fill_manual(values=c("#FCC90C", "#336B91")) +
+  coord_cartesian(ylim = c(0, 1)) +
+  labs(color = "Adjustment strategy", fill = "Adjustment strategy") +
+  facet_grid(comparison~database) +
+  theme(legend.position = "top",
+        legend.direction = "horizontal",
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(color = gray(0.8)),
+        panel.grid.major.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_line(color = gray(0.8)),
+        strip.background = element_blank())
+ggsave(file.path(plotsAndTablesFolder, "Balance_all.png"), width = 10, height = 6, dpi = 300)
